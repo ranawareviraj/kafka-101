c4a980eacae805aa81f2583332813c9a9125f879
ranawareviraj <viraj.b.ranaware@gmail.com>
2024-06-28 14:13:26 -0500

    Updated Readme file

--- src/main/java/com/example/kafka101/kafka/JsonKafkaProducer.java
+++ src/main/java/com/example/kafka101/kafka/JsonKafkaProducer.java
@@ -5,9 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
+//import org.springframework.kafka.support.KafkaHeaders;
+//import org.springframework.messaging.Message;
+//import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -27,14 +27,16 @@ public class JsonKafkaProducer {
     public void sendMessage(User user) {
         LOGGER.info("###### Producing message ######");
         LOGGER.info(String.format("#### Message -> %s", user));
+        kafkaTemplate.send(topic, user);
+        LOGGER.info(String.format("###### Message Published to Kafka Topic -> %s ######", topic));
 
+    /*  Alternatively,
         Message<User> message = MessageBuilder
                 .withPayload(user)
                 .setHeader(KafkaHeaders.TOPIC, topic)
                 .build();
+        kafkaTemplate.send(message);
+    */
 
-        kafkaTemplate.send(topic, user);
-        LOGGER.info(String.format("###### Message Published to Kafka Topic -> %s ######", topic));
-        System.out.println();
     }
 }
